00b4efbcb626d2c2375ba0a4130922ef5b48a235
kazu-S-1110 <xtpgdmw3690@gmail.com>
2022-07-18 22:54:07 +0900

    from,toで基本の使い方を学習。

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { animated, useSpring } from "@react-spring/web"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const styles = useSpring({
+    from:{
+      opacity:0,
+      color: "pink",
+      // translateY: 20,
+      y:500,//上と同じ意味
+      scale:0.5,
+    },
+    to:{
+      opacity:1,
+      color: "blue",
+      y: 0,
+      scale:1,
+    }
+  })
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <animated.h1 style={styles}>Hello Spring</animated.h1>
     </div>
   )
 }
